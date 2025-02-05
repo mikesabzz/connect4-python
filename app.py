3294aad740e2fe37b47f10258e3d5e2d53debfab
Michael Sabzevari <michaelsabzevari@michaels-air.mynetworksettings.com>
2025-02-04 23:36:23 -0500

    some python implemented

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    player1_score = 0
+    player2_score = 0
+    return render_template("index.html", player1_score = player1_score, player2_score = player2_score)
 
 if __name__ == "__main__":
     app.run(debug=True)
